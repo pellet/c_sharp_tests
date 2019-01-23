8b4131acd1935a6461697d23dab9f090f5dce78c
Ben Pettit <ben.pettit@smartcaptech.com>
2019-01-23 11:53:15 +1000

    rename project

---
+++ c_sharp_seven_tests/TupleManager.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CSharpTupleTest
+{
+    public class TupleManager
+    {
+        public delegate (int x, string y) GetTuple();
+
+        private readonly Lazy<(int x, string y)> tuple = 
+            new Lazy<(int x, string y)>(() => 
+                (x:1, y:"2"));
+
+        public GetTuple TupleService =>
+            () => this.tuple.Value;
+    }
+}
