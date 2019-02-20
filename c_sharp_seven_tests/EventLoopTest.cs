f025077ef0baa071f2e9dbdf3dbd0686af0cb168
Ben Pettit <ben.pettit@smartcaptech.com>
2019-02-20 13:16:14 +1000

    another test

--- c_sharp_seven_tests/EventLoopTest.cs
+++ c_sharp_seven_tests/EventLoopTest.cs
@@ -31,5 +31,29 @@ namespace CSharpTupleTest
                 actual: list);
             
         }
+        
+        [Fact]
+        public void lambda_does_not_block_scheduler()
+        {
+            var countdownEvent = new CountdownEvent(1);
+            var list = new List<int>();
+            var scheduler = new EventLoopScheduler();
+            scheduler.Schedule(
+                () => list.Add(1));
+            scheduler.Schedule(
+                TimeSpan.FromSeconds(1),
+                () =>
+                {
+                    list.Add(3);
+                    countdownEvent.Signal();
+                });
+            scheduler.Schedule(
+                () => list.Add(2));
+            countdownEvent.Wait(timeout: TimeSpan.FromSeconds(2));
+            Assert.Equal(
+                expected: new List<int>{1, 2, 3},
+                actual: list);
+            
+        }
     }
 }
