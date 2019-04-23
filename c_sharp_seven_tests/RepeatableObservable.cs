4d61fd3f2a0a4f609df3f08387fb94d694f2b6ed
Ben Pettit <ben.pettit@smartcaptech.com>
2019-04-23 11:24:13 +1000

    a repeat test

--- c_sharp_seven_tests/RepeatableObservable.cs
+++ c_sharp_seven_tests/RepeatableObservable.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Reactive.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CSharpTupleTest
 {
     public class RepeatableObservable
     {
+        private ITestOutputHelper outputHelper;
+
+        public RepeatableObservable(ITestOutputHelper outputHelper) => 
+            this.outputHelper = outputHelper;
+        
         [Fact]
         void repeat_observable()
         {
-            
+            var i = -3;
             var repeatMe =
                 Observable
-                    .Range(0, 4)
-                    .
+                    .Range(1, 4)
+                    .Do(j => i++)
+                    .Do(j => this.outputHelper.WriteLine(j.ToString()))
+                    .Repeat();
+            repeatMe
+                .TakeWhile(j => i < j)
+                .Subscribe();
+            
         }
     }
 }
